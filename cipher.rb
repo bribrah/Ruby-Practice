09df67ffa175fc4ac92cb08f64f74b9531399b79
bribrah <cheesemas46@gmail.com>
2018-11-23 15:38:00 -0800

    added descriptions of exercise to top of code

--- cipher.rb
+++ cipher.rb
@@ -1,3 +1,10 @@
+=begin
+Implement a caesar cipher that takes in a string and the shift factor and then 
+outputs the modified string:
+  > caesar_cipher("What a string!", 5)
+  => "Bmfy f xywnsl!"
+=end
+
 def cipher(str,num)
     str_array = str.split("")
     ciphered_str = ""
